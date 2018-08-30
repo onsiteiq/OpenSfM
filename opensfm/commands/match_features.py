9ce519e18bf0d0386a1b5ab0b3b3ecb7c8a7a501
John Mollis <jmollis@onsiteiq.io>
2018-08-30 20:32:58 +0000

    missed line in intial merge

--- opensfm/commands/match_features.py
+++ opensfm/commands/match_features.py
@@ -202,7 +202,7 @@ def match_candidates_from_metadata(images, exifs, data):
         d = match_candidates_by_distance(images, exifs, reference,
                                          gps_neighbors, max_distance)
         t = match_candidates_by_time(images, exifs, time_neighbors)
-        o = match_candidates_by_order(images, order_neighbors)
+        o = match_candidates_by_order(images, order_neighbors, data)
         pairs = d | t | o
 
     res = {im: [] for im in images}
